80543f430a4dd5efcec3b9e6da3058675a3dcea1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2024-09-01 16:38:00 +0800

    Rename mydrivers.rss to mydrivers.xml

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import fs from 'fs/promises';
 import lib from './lib.js';
 const feed = await lib.fetchFilter();
 if (feed) {
-	await fs.writeFile('mydrivers.rss', feed);
+	await fs.writeFile('mydrivers.xml', feed);
 }
