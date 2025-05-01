5148f878c20f5f4beb63719f89629d9463f58120
Jacky Lee <JackyLeeHongJian@Gmail.com>
2025-05-01 11:54:37 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,17 @@
+let titleArr = [
+    '9元',
+    '19元',
+    '199元',
+    '1999元',
+    '19999元',
+    '9.99元',
+    '19.99元',
+    '199.99元',
+    '1999.99元',
+    '19999.99元',
+];
+titleArr = titleArr.concat(titleArr.map(title => `优${title}`))
+const regexArr = [ /^[^0-9]?[0-9]{1,2}([.][0-9]{1,2})?\s?元/ ];
+titleArr.forEach(title => {
+    console.log(title, !regexArr[0].test(title));
+});
